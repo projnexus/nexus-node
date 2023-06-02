969b28436c53ea4e58e5c48c4e5e90e2a1df717f
Lvmostwanted <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-02 01:29:35 +0100

    New admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,7 +1,26 @@
 import { apiRequestWithToken } from '../utils/request';
 import { Client } from '../models/Client';
 
+export const getKey = async function (this: Client, key: string): Promise<{ response: Response; status: number }> {
+  if (!this) throw new Error('Client not initialized');
+  return await apiRequestWithToken(`admin/key/${key}`, this.token);
+};
+
 export const createKey = async function (this: Client, data: object): Promise<{ response: Response; status: number }> {
   if (!this) throw new Error('Client not initialized');
   return await apiRequestWithToken(`admin/key`, this.token, 'POST', data);
+};
+
+export const deleteKey = async function (this: Client, key: string): Promise<{ response: Response; status: number }> {
+  if (!this) throw new Error('Client not initialized');
+  return await apiRequestWithToken(`admin/key/${key}`, this.token, 'DELETE');
+};
+
+export const updateKey = async function (
+  this: Client,
+  key: string,
+  data: object,
+): Promise<{ response: Response; status: number }> {
+  if (!this) throw new Error('Client not initialized');
+  return await apiRequestWithToken(`admin/key/${key}`, this.token, 'PATCH', data);
 };
