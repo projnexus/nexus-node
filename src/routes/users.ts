984f2a157b67529b8cfb9b09ba31ac95e26a53bb
Lvmostwanted <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-02 01:25:47 +0100

    User JSDOC

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,16 +2,39 @@ import { apiRequestWithToken } from '../utils/request';
 import { Client } from '../models/Client';
 import { User } from '../models/User';
 
+/**
+ * Get the amount of users Nexus has
+ *
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const getUserCount = async function (this: Client): Promise<{ response: Response; status: number }> {
   if (!this) throw new Error('Client not initialized');
   return await apiRequestWithToken('users/count', this.token);
 };
 
+/**
+ * Get all Nexus' users
+ *
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const getAllUsers = async function (this: Client): Promise<{ response: Response; status: number }> {
   if (!this) throw new Error('Client not initialized');
   return apiRequestWithToken('users', this.token);
 };
 
+/**
+ * Get the information of a user
+ *
+ * @param userId - optional
+ * @param identifiers - optional
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const getUser = async function (
   this: Client,
   userId?: string,
@@ -24,11 +47,28 @@ export const getUser = async function (
   throw new Error('Invalid state: No matching condition found');
 };
 
-export const deleteUser = async function (this: Client, user: User): Promise<{ response: Response; status: number }> {
+/**
+ * Delete a user
+ *
+ * @param userId
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
+export const deleteUser = async function (this: Client, userId: string): Promise<{ response: Response; status: number }> {
   if (!this) throw new Error('Client not initialized');
-  return apiRequestWithToken(`user/${user.id}`, this.token, 'DELETE');
+  return apiRequestWithToken(`user/${userId}`, this.token, 'DELETE');
 };
 
+/**
+ * Update a user
+ *
+ * @param user
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ * @see {@link User}
+ */
 export const updateUser = async function (
     this: Client,
     user: User,
