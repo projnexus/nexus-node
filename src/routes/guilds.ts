4539e2f1644a3c8df11e00134985bcf3b908786d
Lvmostwanted <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-02 01:13:35 +0100

    Guild route JSDOC

--- src/routes/guilds.ts
+++ src/routes/guilds.ts
@@ -2,11 +2,26 @@ import { apiRequestWithToken } from '../utils/request';
 import { Client } from '../models/Client';
 import { GuildSettings } from '../models/GuildSettings';
 
+/**
+ * Get the amount of guilds Nexus is in
+ *
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const getGuildCount = async function (this: Client): Promise<{ response: Response; status: number }> {
     if (!this) throw new Error('Client not initialized');
     return await apiRequestWithToken('guilds/count', this.token);
 }
 
+/**
+ * Get the information of a guild
+ *
+ * @param guildId
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const getGuild = async function (
   this: Client,
   guildId: string,
@@ -15,6 +30,15 @@ export const getGuild = async function (
   return await apiRequestWithToken(`guild/${guildId}`, this.token);
 };
 
+/**
+ * Create a new guild
+ *
+ * @param guildSettings
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ * @see {@link GuildSettings}
+ */
 export const createGuild = async function (
   this: Client,
   guildSettings: GuildSettings,
@@ -23,6 +47,14 @@ export const createGuild = async function (
   return await apiRequestWithToken(`guild`, this.token, 'POST', guildSettings.toJson());
 };
 
+/**
+ * Delete a guild
+ *
+ * @param guildId
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ */
 export const deleteGuild = async function (
   this: Client,
   guildId: string,
@@ -31,6 +63,16 @@ export const deleteGuild = async function (
   return await apiRequestWithToken(`guild/${guildId}`, this.token, 'DELETE');
 };
 
+/**
+ * Update a guild
+ *
+ * @param guildId
+ * @param guildSettings
+ * @memberof Client
+ * @throws {Error} Client not initialized
+ * @returns {Promise<{ response: Response; status: number }>}
+ * @see {@link GuildSettings}
+ */
 export const updateGuild = async function (
   this: Client,
   guildId: string,
