4b2ec415dcae4e63e973233029ed2df5ff0ed8a3
Lvmostwanted <48731374+Lvmostwanted@users.noreply.github.com>
2023-06-02 02:13:55 +0100

    Updated [unauthenticated] client test

--- src/__tests__/unauthenticated/models/Client.test.ts
+++ src/__tests__/unauthenticated/models/Client.test.ts
@@ -1,45 +1,70 @@
-// import { Client } from "../../../models/Client";
-//
-// describe('Client', () => {
-//   describe('Is using token', () => {
-//     test('False', () => {
-//       const client = new Client('1234', false, true);
-//       expect(client.useToken).toBe(false);
-//     });
-//
-//     test('True', () => {
-//       const client = new Client('1234', true, true);
-//       expect(client.useToken).toBe(true);
-//     });
-//   });
-//
-//   test('Get token', () => {
-//     const client = new Client('1234', false, true);
-//     expect(client.token).toBe('1234');
-//   });
-//
-//   describe('Validate token', () => {
-//     test('Valid', () => {
-//       const client = new Client('1234', true, true);
-//       expect(client.validateToken()).toBe(true);
-//     });
-//
-//     test('Invalid', () => {
-//       const client = new Client('', true, true);
-//       expect(client.validateToken()).toBe(false);
-//     });
-//   });
-//
-//   describe('Authenticate', () => {
-//     test('Invalid', async () => {
-//       const client = new Client('', false, true);
-//       const authenticated = await client.authenticate();
-//       expect(authenticated).toBe(false);
-//     });
-//     // testroute('Valid', async () => {
-//     //   const client = new Client('', true, true);
-//     //   const authenticated = await client.authenticate();
-//     //   expect(authenticated).toBe(true);
-//     // });
-//   });
-// });
+import { Client } from '../../../models/Client';
+
+describe('Client', () => {
+  describe('Is using token', () => {
+    test('False', () => {
+      const client = new Client({
+        token: '1234',
+        useToken: false,
+        silent: true,
+      });
+
+      expect(client.useToken).toBe(false);
+    });
+
+    test('True', () => {
+      const client = new Client({
+        token: '1234',
+        useToken: false,
+        silent: true,
+      });
+
+      expect(client.useToken).toBe(true);
+    });
+  });
+
+  test('Get token', () => {
+    const client = new Client({
+      token: '1234',
+      useToken: false,
+      silent: true,
+    });
+
+    expect(client.token).toBe('1234');
+  });
+
+  describe('Validate token', () => {
+    test('Valid', () => {
+      const client = new Client({
+        token: '1234',
+        useToken: true,
+        silent: true,
+      });
+
+      expect(client.validateToken()).toBe(true);
+    });
+
+    test('Invalid', () => {
+      const client = new Client({
+        token: '',
+        useToken: true,
+        silent: true,
+      });
+
+      expect(client.validateToken()).toBe(false);
+    });
+  });
+
+  describe('Authenticate', () => {
+    test('Invalid', async () => {
+      const client = new Client({
+        token: '1234',
+        useToken: true,
+        silent: true,
+      });
+
+      const authenticated = await client.authenticate();
+      expect(authenticated).toBe(false);
+    });
+  });
+});
